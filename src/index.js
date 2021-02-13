1d96c6edcd3b3ab08c23c396ddc5608ebbffb6c1
Taylor Johnson <taylorjohnson141@gmail.com>
2021-02-13 12:17:14 -0700

    Add initial board

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let gameBoard = document.getElementById('Game-board')
+let gameBoard = document.getElementById('game-board')
 const ctx = gameBoard.getContext('2d');
 ctx.fillStyle = 'green';
-ctx.fillRect(0, 20, 900, 900)
+ctx.fillRect(0, 0, 450, 450);
